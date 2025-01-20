022a32d8f73421b6d60029ed194490a00007ad2c
Niccolò Albanese <nick_albanese@virgilio.it>
2025-01-20 16:48:46 +0100

    Update Esercizi.sql

--- Esercizi.sql
+++ Esercizi.sql
@@ -7,3 +7,14 @@ WHERE EXTRACT (YEAR FROM anno_di_nascita) = 1980;
 
 SELECT * FROM Fatture
 WHERE Iva = 22;
+
+SELECT * FROM Prodotti
+WHERE EXTRACT (YEAR FROM data_attivazione) = 2023 AND (in_produzione = true OR in_commercio) = true;
+
+SELECT *
+FROM public.Fatture
+JOIN public.Clienti
+ON id_cliente = numero_cliente
+WHERE importo < 4000
+ORDER BY numero_fattura;
+
